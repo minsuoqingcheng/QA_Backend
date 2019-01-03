49f84ced502ded0482ae78edd8ea3e218b20de37
chengliu <1397004810@qq.com>
2019-01-03 19:53:40 +0800

    【develop】 add pom and init the project main class

---
+++ src/main/java/cn/nju/edu/se/QAApplication.java
@@ -0,0 +1,19 @@
+package cn.nju.edu.se;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QAApplication {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(QAApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(QAApplication.class, args);
+        LOGGER.info("Q&A service is running!");
+    }
+
+
+}
