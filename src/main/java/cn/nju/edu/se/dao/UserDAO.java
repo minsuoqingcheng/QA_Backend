e99eca1a3e03ef94ab36dfda03f70c90d5f3d3eb
chengliu <1397004810@qq.com>
2019-01-14 23:13:54 +0800

    【develop】 user dao and service add

---
+++ src/main/java/cn/nju/edu/se/dao/UserDAO.java
@@ -0,0 +1,24 @@
+package cn.nju.edu.se.dao;
+
+import cn.nju.edu.se.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.Serializable;
+import java.util.List;
+
+public interface UserDAO extends JpaRepository<User, Integer>, JpaSpecificationExecutor<User>, Serializable {
+
+    List<User> findAllByNickName(String nickName);
+
+    @Transactional
+    void deleteByNickName(String nickName);
+    @Transactional
+    User save(User user);
+
+    User findById(int userId);
+
+
+
+}
