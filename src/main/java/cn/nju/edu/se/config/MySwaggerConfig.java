4c7487121b1d34470843f016f3fbfab0338b1efa
chengliu <1397004810@qq.com>
2019-01-22 22:18:22 +0800

    【develop】 swagger config modify

--- src/main/java/cn/nju/edu/se/config/MySwaggerConfig.java
+++ src/main/java/cn/nju/edu/se/config/MySwaggerConfig.java
@@ -2,11 +2,8 @@ package cn.nju.edu.se.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
@@ -15,27 +12,20 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class MySwaggerConfig extends WebMvcConfigurationSupport {
+public class MySwaggerConfig {
+    public MySwaggerConfig() {
+    }
 
     @Bean
-    public Docket createRestApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.nju.edu.se.controller"))
-                .paths(PathSelectors.any())
-                .build();
+    public Docket customDocket() {
+        return (new Docket(DocumentationType.SWAGGER_2)).apiInfo(this.apiInfo()).useDefaultResponseMessages(false).select().apis((input) -> {
+            Class<?> declaringClass = input.declaringClass();
+            return declaringClass.isAnnotationPresent(RestController.class);
+        }).build();
     }
 
     private ApiInfo apiInfo() {
-        Contact contact = new Contact("admin", "666", "admin");
-        return (new ApiInfoBuilder()).title("后台API接口").description("后台台API接口").contact(contact).version("1.1.0").build();
+        Contact contact = new Contact("", "", "");
+        return (new ApiInfoBuilder()).title("后台API接口").description("后台API接口").contact(contact).version("1.1.0").build();
     }
-
-
-    @Override
-    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**").addResourceLocations("classpath:/META-INF/resources/").setCachePeriod(0);
-    }
-
 }
